4a14bd91d7c927d74ed6d37362b93513fd1f5bf9
dharroyave <duvan.arroyave03@gmail.com>
2025-11-10 20:25:43 -0500

    Creacion

---
+++ server.js
@@ -0,0 +1,9 @@
+// se ejecuta cuando queremos iniciar el servidor backend
+import app from "./app.js";
+
+const port = process.env.PORT || 3001
+
+// 3. ejecutar el servidor en nuestro computador
+app.listen(port, () => {
+    console.log('El servidor está ejecutándose correctamente, en el puerto ', port);
+});
